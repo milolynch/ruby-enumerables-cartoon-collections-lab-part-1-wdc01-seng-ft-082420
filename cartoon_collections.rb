e68dd9d3142e6e275dbe63679ff71288f2432ef6
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-16 10:47:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,12 +2,16 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each {|char| puts"Hi #{char}"}
+  array.each do |char|
+    puts "Hi #{char}"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index {|item, index| {puts "#{index} #{item}"}
+  array.each_with_index do |item, index|
+    puts "#{index} #{item}"
+  end
 end
